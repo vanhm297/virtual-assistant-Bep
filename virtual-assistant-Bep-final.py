92e99a289e85461d12d0e439e84649551a53814c
unknown <vannguyenthu2907@gmail.com>
2021-03-01 14:25:44 +0700

    for Thao

--- virtual-assistant-Bep-final.py
+++ virtual-assistant-Bep-final.py
@@ -4,6 +4,9 @@ import random
 import time
 from datetime import date
 from time import strftime
+import webbrowser
+import json
+from youtube_search import YoutubeSearch
 
 #bep brain
 bep_brain = ""
@@ -25,21 +28,6 @@ def bep_mouth_setting(bep_brain):
     bep_mouth.runAndWait()
 
 
-#recommend a taylor swift song
-def recommend_TS_song():
-    if "happy" in you:
-        with open('TS-happy-songs.txt','r') as file:
-            happySong = file.read().split("\n*")
-        song_Recommend = str(random.choice(happySong))
-    elif "sad" in you:
-        with open('TS-sad-songs.txt','r') as file:
-            sadSong = file.read().split("\n*")
-        song_Recommend = str(random.choice(sadSong))
-    else:
-        bep_mouth_setting("Sorry I can't help you this time")
-    bep_mouth_setting(song_Recommend)
-
-
 #greetings based on local time
 def hello():
     day_time = int(strftime('%H'))
@@ -51,10 +39,37 @@ def hello():
         greeting = "Good evening"
     return greeting
 
-greeting = hello()
+
+#recommend a taylor swift song
+def recommend_TS_song():
+    if "happy" in you :
+        with open('TS-happy-songs.txt','r') as file:
+            happySong = file.read().split("\n*")
+        song_Recommend = str(random.choice(happySong))
+    elif "sad" in you :
+        with open('TS-sad-songs.txt','r') as file:
+            sadSong = file.read().split("\n*")
+        song_Recommend = str(random.choice(sadSong))
+    else:
+        bep_mouth_setting("Sorry I can't help you this time")
+    return song_Recommend
+
+
+#play the song on Youtube
+def play_on_Youtube():
+    bep_mouth_setting("Do you want to play it?")
+    you = bep_ear_setting()
+    if "yes" in you:
+        result = YoutubeSearch(song_Recommend, max_result=1).to_json()
+        url = json.loads(result)
+        id_url = url["videos"][0]["id"]
+        webbrowser.get("chrome %s").open("https://youtu.be?" + id_url)
+    else:
+        bep_mouth_setting("Okay")
 
 
 #intro
+greeting = hello()
 bep_brain = greeting + ". I'm James. What is your name?"
 bep_mouth_setting(bep_brain)
 you = bep_ear_setting()
@@ -64,20 +79,22 @@ bep_mouth_setting(bep_brain)
 you = bep_ear_setting()
 
 #Functions
-if "today" in you:
-    today = date.today()
-    d2 = today.strftime("Today is %B %d, %Y")
-    bep_mouth_setting(d2)
+while True:
+    if "today" in you:
+        today = date.today()
+        d2 = today.strftime("Today is %B %d, %Y")
+        bep_mouth_setting(d2)
 
-if "song" in you:
-    bep_mouth_setting("a Happy or a Sad song?")
-    you = bep_ear_setting()
-    recommend_TS_song()
+    if "song" in you:
+        bep_mouth_setting("a happy or a sad song?")
+        you = bep_ear_setting()
+        recommend_TS_song()
+        song_Recommend = recommend_TS_song()
 
-else:
-    bep_mouth_setting("Sorry I can't hear you. Say again")
-    you = bep_ear_setting()
-    
-    
+    else:
+        bep_mouth_setting("Sorry I can't hear you. Say again")
+        you = bep_ear_setting()
+
+
 
 input()
